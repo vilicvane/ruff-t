1cf2dbcade7ff50b83e875ca2b9595d895183c21
vilicvane <i@vilic.info>
2016-04-22 12:42:57 +0800

    Fix global declaration file

--- t.d.ts
+++ t.d.ts
@@ -1,11 +1,22 @@
-import {
-    ScopeHandler,
-    GeneralHandler
-} from './bld/';
+declare namespace T {
+    interface Scope {
+        timeout: number;
+        describe(description: string, handler: ScopeHandler): void;
+        it(description: string, handler: GeneralHandler): void;
+        before(handler: GeneralHandler): void;
+        beforeEach(handler: GeneralHandler): void;
+        after(handler: GeneralHandler): void;
+        afterEach(handler: GeneralHandler): void;
+    }
 
-export declare function describe(description: string, handler: ScopeHandler): void;
-export declare function it(description: string, handler: GeneralHandler): void;
-export declare function before(handler: GeneralHandler): void;
-export declare function beforeEach(handler: GeneralHandler): void;
-export declare function after(handler: GeneralHandler): void;
-export declare function afterEach(handler: GeneralHandler): void;
+    type DoneCallback = (error?: any) => void;
+    type ScopeHandler = (scope: Scope) => void;
+    type GeneralHandler = (done?: DoneCallback) => Promise<void> | void;
+}
+
+declare function describe(description: string, handler: T.ScopeHandler): void;
+declare function it(description: string, handler: T.GeneralHandler): void;
+declare function before(handler: T.GeneralHandler): void;
+declare function beforeEach(handler: T.GeneralHandler): void;
+declare function after(handler: T.GeneralHandler): void;
+declare function afterEach(handler: T.GeneralHandler): void;
